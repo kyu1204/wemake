f0a020d40d82abe10077478ab00672561dcd43b4
MINT <pb1123love@gmail.com>
2025-03-31 22:17:19 +0900

    init

---
+++ app/potato/tomato.tsx
@@ -0,0 +1,17 @@
+export default function AboutUs() {
+  return <div>AboutUs</div>;
+}
+
+export const meta = () => [
+  {
+    title: "About Us",
+    description: "About Us",
+  },
+];
+
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: "/potato/tomato.css",
+  },
+];
