b5ffd9947a69874a77c106db29aa44828706427d
MINT <pb1123love@gmail.com>
2025-04-02 21:56:31 +0900

    ✨ feat: 내비게이션 컴포넌트를 추가하고 의존성 업데이트
    내비게이션 컴포넌트를 추가하여 사용자 경험을 향상시킵니다. 
    또한, Radix UI 라이브러리의 새로운 버전을 추가하여 
    기능성을 개선하고 최신 기능을 활용합니다. 
    불필요한 dev 의존성을 제거하여 패키지 크기를 줄입니다.

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-
+import Navigation from "./common/components/navigation";
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -33,6 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Navigation />
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -42,7 +43,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+    </>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
