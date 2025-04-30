6a860e54312a7f70e60c32f67671bd5bad8b4d23
MINT <pb1123love@gmail.com>
2025-04-30 23:13:35 +0900

    ✨ feat: 아이디어 페이지 및 상세 페이지 추가
    아이디어 목록과 상세 내용을 보여주는 새로운 라우트를 추가한다.  
    사용자가 프로젝트 아이디어를 쉽게 탐색하고 상세 정보를 확인할 수 있도록  
    아이디어 페이지와 개별 아이디어 페이지를 구현하였다.

---
+++ app/features/ideas/pages/idea-page.tsx
@@ -0,0 +1,43 @@
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/idea-page";
+import { DotIcon, EyeIcon, HeartIcon } from "lucide-react";
+import { Button } from "~/common/components/ui/button";
+
+export const meta: Route.MetaFunction = () => {
+  return [
+    {
+      title: "IdeaGPT | wemake",
+      description: "Find ideas for your next project",
+    },
+  ];
+};
+
+export default function IdeaPage() {
+  return (
+    <div>
+      <Hero title="IdeaGPT #12312312312" />
+      <div className="max-w-screen-sm mx-auto flex flex-col items-center gap-10">
+        <p className="italic text-center">
+          A Startup that creates an AI-powered generated personal trainer,
+          delivering customized fitness recommendations based on the user's
+          goals and preferences and also provides a community for users to
+          connect and share their fitness journey.
+        </p>
+        <div className="flex items-center text-sm">
+          <div className="flex items-center gap-1">
+            <EyeIcon className="size-4" />
+            <span>123</span>
+          </div>
+          <DotIcon className="size-4" />
+          <span>10 hours ago</span>
+          <DotIcon className="size-4" />
+          <Button variant="outline">
+            <HeartIcon className="size-4" />
+            <span>12</span>
+          </Button>
+        </div>
+        <Button size="lg">Claim idea now &rarr;</Button>
+      </div>
+    </div>
+  );
+}
