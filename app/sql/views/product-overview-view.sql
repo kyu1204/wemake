7f4dd5ad8ce35a870653772fe0e5cd9dedd7a188
MINT <pb1123love@gmail.com>
2025-05-30 00:10:45 +0900

    feat: 제품 개요 페이지에 동적 데이터 로딩 추가
    - product-overview-layout에 loader와 meta 함수 추가하여 제품 데이터를
      서버에서 받아오도록 변경
    - 제품 이름, 태그라인, 평점, 리뷰 수, 업보트 수 등 동적 정보 표시 구현
    - product-overview에서 하드코딩된 설명을 useOutletContext로 전달받은
      실제 데이터로 대체
    - supa-client에 product_overview_view 타입 추가로 타입 안정성 강화
    - URL 경로에 제품 ID를 반영하여 라우팅 동적 처리 가능하게 수정
    
    이 변경으로 제품 개요 페이지가 실제 API 데이터를 기반으로 동작하며,
    유저에게 정확한 제품 정보를 제공할 수 있게 됨

---
+++ app/sql/views/product-overview-view.sql
@@ -0,0 +1,19 @@
+create or replace view product_overview_view as
+select
+   product_id,
+   name,
+   tagline,
+   description,
+   how_it_works,
+   icon,
+   url,
+   stats->>'upvotes' as upvotes,
+   stats->>'views' as views,
+   stats->>'reviews' as reviews,
+   avg(product_reviews.rating) as average_rating
+from public.products
+left join public.reviews as product_reviews using (product_id)
+group by product_id;
+
+
+select * from product_overview_view;
