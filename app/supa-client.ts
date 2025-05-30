4324ad1356b6c6c739c818759996a8de952a66f5
MINT <pb1123love@gmail.com>
2025-05-31 00:07:46 +0900

    feat: community_post_detail_view 참조 관계 추가
    여러 테이블에 community_post_detail_view를 참조하는 외래키 관계를 추가한다.  
    이 변경으로 데이터 무결성을 강화하고, 커뮤니티 게시물 상세 정보와 프로필 간의  
    연결을 명확히 하여 쿼리 최적화 및 데이터 연동을 개선한다.

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -16,6 +16,15 @@ type Database = MergeDeep<
             string | null
           >;
         };
+        community_post_detail_view: {
+          Row: SetFieldType<
+            SetNonNullable<
+              SupabaseDatabase["public"]["Views"]["community_post_detail_view"]["Row"]
+            >,
+            "author_avatar",
+            string | null
+          >;
+        };
         gpt_ideas_view: {
           Row: SetNonNullable<
             SupabaseDatabase["public"]["Views"]["gpt_ideas_view"]["Row"]
